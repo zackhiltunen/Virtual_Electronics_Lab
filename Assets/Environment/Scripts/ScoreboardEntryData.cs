cea4e365e24076cd6d8a5f581edbf3442efa0a90
zackhiltunen <59694520+zackhiltunen@users.noreply.github.com>
2021-03-13 17:56:36 -0500

    added initial scoreboard object

---
+++ Assets/Environment/Scripts/ScoreboardEntryData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs4910.Scoreboards
+{
+    [Serializable]
+    public struct ScoreboardEntryData
+    {
+        public string entryName;
+        public int entryScore;
+    }
+}
+
---
+++ Assets/Environment/Scripts/ScoreboardEntryUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace cs4910.Scoreboards
+{
+    public class ScoreboardEntryUI : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI entryNameText = null;
+        [SerializeField] private TextMeshProUGUI entryScoreText = null;
+
+        public void Initialize(ScoreboardEntryData scoreboardEntryData)
+        {
+            entryNameText.text = scoreboardEntryData.entryName;
+            entryScoreText.text = scoreboardEntryData.entryScore.ToString();
+        }
+    
+    }
+}
