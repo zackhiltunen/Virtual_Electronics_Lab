4d17fc9f74f2edc00e1ad4b0c57d16ee0c880bb2
zackhiltunen <59694520+zackhiltunen@users.noreply.github.com>
2021-04-13 15:54:57 -0400

    updated score script

---
+++ Assets/Environment/Scripts/Score.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace cs4910.Scoreboards
+{
+    public class Score : MonoBehaviour
+    {
+        public Timer timer;
+        public TextMeshProUGUI score;
+
+        float time;
+        public void Start()
+        {
+            score.text = "100";
+        }
+
+        public void Update()
+        {
+            if (timer.time >= 60)
+            {
+                score.text = "80";
+            }
+        }
+    }
+}
